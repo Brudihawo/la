76c994a53269d567b6e670615c609a2551753036
Hawo Höfer <hawo.hoefer98@freenet.de>
2022-03-18 16:39:07 +0100

    [testing] Add test util for test macros

---
+++ test/test_util.h
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+#define RED   "\033[1;31m"
+#define RED_N "\033[0;31m"
+#define GRN   "\033[1;32m"
+#define GRN_N "\033[0;32m"
+#define RST   "\033[0m"
+
+#define TEST_PASS(name) \
+  fprintf(stdout, GRN "PASSED: %s in %s %s:%i"RST"\n",\
+      name, __func__, __FILE__, __LINE__)
+#define TEST_FAIL(name) \
+  fprintf(stdout, RED "FAILED: %s in %s %s:%i"RST"\n",\
+      name, __func__, __FILE__, __LINE__)
+
+#define TEST_FAIL_MSG(name, msg, ...) \
+  fprintf(stdout, RED "FAILED: %s in %s %s:%i"RST"\n" msg "\n",\
+      name, __func__, __FILE__, __LINE__, __VA_ARGS__)
+
+#define TEST(name, cond) cond; cond ? TEST_PASS(name) : TEST_FAIL(name);
