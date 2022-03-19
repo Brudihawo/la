3cca9cdf779d9c1866c76d858bfbb58acbe88250
Hawo Höfer <hawo.hoefer98@freenet.de>
2022-03-19 13:42:32 +0100

    [testing] better testing output formatting

--- test/test_util.h
+++ test/test_util.h
@@ -7,14 +7,14 @@
 #define RST   "\033[0m"
 
 #define TEST_PASS(name) \
-  fprintf(stdout, GRN "PASSED: %s in %s %s:%i"RST"\n",\
+  fprintf(stdout, GRN "PASSED: %s in %s\n"GRN_N"  %s:%i"RST"\n",\
       name, __func__, __FILE__, __LINE__)
 #define TEST_FAIL(name) \
-  fprintf(stdout, RED "FAILED: %s in %s %s:%i"RST"\n",\
+  fprintf(stdout, RED "FAILED: %s in %s\n"RED_N"  %s:%i"RST"\n",\
       name, __func__, __FILE__, __LINE__)
 
 #define TEST_FAIL_MSG(name, msg, ...) \
-  fprintf(stdout, RED "FAILED: %s in %s %s:%i"RST"\n" msg "\n",\
+  fprintf(stdout, RED "FAILED: %s in %s\n"RED_N"  %s:%i"RST"\n  " msg "\n",\
       name, __func__, __FILE__, __LINE__, __VA_ARGS__)
 
 #define TEST(name, cond) cond; cond ? TEST_PASS(name) : TEST_FAIL(name);
